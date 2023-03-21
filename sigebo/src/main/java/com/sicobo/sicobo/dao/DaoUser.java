a4b2c17ae01e36d0d4074359bcc13563fc844f6e
HectorUlisesStamatio <ulisesstamatio@gmail.com>
2023-03-21 14:56:35 -0600

    [✏️UPDATE ] inicio de sesión - seguridad - implementación de captcha

--- sigebo/src/main/java/com/sicobo/sicobo/dao/DaoUser.java
+++ sigebo/src/main/java/com/sicobo/sicobo/dao/DaoUser.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DaoUser  extends JpaRepository<BeanUser, Long> {
     BeanUser findByUsername(String username);
+    boolean existsBeanUserByUsername(String username);
 }
