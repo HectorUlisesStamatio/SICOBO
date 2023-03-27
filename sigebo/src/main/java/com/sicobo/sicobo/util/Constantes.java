f7aca5e3a854c5e688a1fc20649e238a6a85bc9a
20203tn096 <84577846+20203tn096@users.noreply.github.com>
2023-03-27 12:50:29 -0600

    [❌FIX] Modulo sitios - correcciones de constantes

--- sigebo/src/main/java/com/sicobo/sicobo/util/Constantes.java
+++ sigebo/src/main/java/com/sicobo/sicobo/util/Constantes.java
@@ -1,5 +1,10 @@
 package com.sicobo.sicobo.util;
 
+import static com.sicobo.sicobo.util.Constantes.Message_Body.*;
+import static com.sicobo.sicobo.util.Constantes.Message_Codes.*;
+import static com.sicobo.sicobo.util.Constantes.Message_Type.*;
+import static com.sicobo.sicobo.util.Constantes.Message_Headers.*;
+
 public final class Constantes {
 
     public static final class Roles {
@@ -12,6 +17,8 @@ public final class Constantes {
         public final static String FAILED_EXECUTION = "Ejecución fallida";
         public final static String SUCCESSFUL_REGISTRATION = "Registro exitoso";
         public final static String SUCCESSFUL_SEARCH = "Búsqueda exitosa";
+
+        public final static String SUCCESSFUL_UPDATE = "Actualización exitosa";
         public final static String FAILED_SEARCH = "Búsqueda fallida";
     }
 
@@ -25,6 +32,14 @@ public final class Constantes {
         public static final String INTERNAL_ERROR ="Ocurrió un error interno";
         public static final String INSERT_SUCCESSFUL="Se ha realizado el registro exitosamente";
         public static final String SEARCH_SUCCESSFUL="Se ha realizado la búsqueda exitosamente";
+        public static final String UPDATE_SUCCESSFUL="Se ha realizado la actualización exitosamente";
+
+        public static final String ERROR_NAME = "Ingresa un valor válido en el campo nombre";
+
+        public static final String ERROR_ADDRESS = "Ingresa un valor válido en el campo direccion";
+
+        public static final String ERROR_STATE = "Ingresa un valor válido en el campo estado";
+
     }
 
     public static final class Message_Type{
@@ -39,6 +54,10 @@ public final class Constantes {
         public static final String REDIRECT_LOGIN = "redirect:/login";
         public static final String GESTOR_BODEGAS = "gestorViews/listWarehouses";
         public static final String REDIRECT_ERROR = "redirect:/error";
+        public static final String ADMIN_LISTSITES = "adminViews/listSites";
+        public static final String ADMIN_REGISTERSITE = "adminViews/registerSite";
+        public static final String REDIRECT_ADMIN_LISTSITES = "redirect:/admin/sitios";
+        public static final String ADMIN_UPDATESITE = "adminViews/updateSite";
     }
 
     public static final class Stuff{
@@ -46,6 +65,18 @@ public final class Constantes {
         public static final String USER = "user";
         public static final String ERRORS = "Error: ";
         public static final String STATUS = "status";
+        public static final String OPTION = "opcion";
+        public static final String RESPONSE = "response";
+        public static final String SITES = "sitios";
+
+        public static final String STATES = "states";
+        public static final String SITE = "site";
+        public static final String MAP_ERROR = "error";
+    }
+
+    public static class ObjectMessages{
+        public static final Message MESSAGE_CATCH_ERROR = new Message(INTERNAL_ERROR, "No se realizó la acción indicada. Inténtalo de nuevo", FAILED, SERVER_FAIL_CODE, null );
+        public static final Message MESSAGE_FIELD_ERRORS = new Message(FAILED_EXECUTION, "Ingresa valores válidos", FAILED, FAIL_CODE, null);
     }
 
 
