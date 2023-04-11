639c59336faea047f41ca1b1a915c00650da16ae
20203tn096 <84577846+20203tn096@users.noreply.github.com>
2023-04-11 16:53:59 -0500

    [✏UPDATE] Se agrego a vencer y a desalojar

--- sigebo/src/main/java/com/sicobo/sicobo/util/PaymentValidator.java
+++ sigebo/src/main/java/com/sicobo/sicobo/util/PaymentValidator.java
@@ -16,7 +16,6 @@ public class PaymentValidator {
     public boolean validDateOut(Date dueDate, Date currentDate){
         long diffInMilliseconds = Math.round(dueDate.getTime() - currentDate.getTime());
         long diffInDays = TimeUnit.MILLISECONDS.toDays(diffInMilliseconds);
-        System.out.println("Diferencia en dias " + diffInDays);
         return diffInDays < -20;
     }
 }
